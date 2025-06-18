dcba7fde998f8d00332462b85aad01c192b28ef8
anriabb <149783052+anriabb@users.noreply.github.com>
2025-06-18 20:32:18 +0400

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "Please run as root or use sudo"
+  exit 1
+fi
+
+
+INTERFACE=$(ip -o link show up | awk -F': ' '{print $2}' | grep -v lo | head -n 1)
+
+if [[ -z "$INTERFACE" ]]; then
+  echo "No suitable network interface found."
+  exit 1
+fi
+
+echo "Selected interface: $INTERFACE"
+
+
+tcpdump -i "$INTERFACE" -n -v 
+
